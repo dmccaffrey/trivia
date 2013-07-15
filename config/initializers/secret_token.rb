f9e084fd039469100e2e247736dcb58cb5926be3
David McCaffrey <root@redxent.org>
2013-07-14 19:47:40 -0500

    Initialized rails project and set default route

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trivia::Application.config.secret_key_base = '6b5c5cb6ab276e51b228086461fdb7f3da2b21fa843691ea798974c2c9d9f13e08cfdf59c0128a650341ce2ac6956528457d10338d1ad36eebfedef66b00aed8'
