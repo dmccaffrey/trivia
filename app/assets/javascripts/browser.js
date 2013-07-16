a2407afe4f0858155b01b4e01e54cf711dd93664
David McCaffrey <root@redxent.org>
2013-07-15 19:37:37 -0500

    Improved look and feel

--- app/assets/javascripts/browser.js
+++ app/assets/javascripts/browser.js
@@ -4,15 +4,30 @@ $(function() {
 	});
 	$("#question-input").val("How much wood could a woodchuck chuck if a woodchuck could chuck wood?").triggerHandler("input");
 	
+	$("#hint-input").bind('input propertychange', function() {
+		if($("#hint-input").val() == "") {
+			$(".card .hint").hide();
+		} else {
+			$(".card .hint").text($("#hint-input").val());
+			$(".card .hint").show();
+		}
+	});
+	$("#hint-input").val("10, 20, 30").triggerHandler("input");
+	
 	$("#answer-input").bind('input propertychange', function() {
 		$(".card .answer").text($("#answer-input").val());
 	});
 	$("#answer-input").val("Twenty").triggerHandler("input");
 	
-	$("#explanation-input").bind('input propertychange', function() {
-		$(".card .explanation").text($("#explanation-input").val());
+	$("#details-input").bind('input propertychange', function() {		
+		if($("#details-input").val() == "") {
+			$(".card .details").hide();
+		} else {
+			$(".card .details").text($("#details-input").val());
+			$(".card .details").show();
+		}
 	});
-	$("#explanation-input").val("This is some kind of credible explanation or something. If you didn't know this you should feel really stupid.").triggerHandler("input");
+	$("#details-input").val("This is some kind of credible explanation or something. If you didn't know this you should feel really stupid.").triggerHandler("input");
 	
 	$("#reference-input").bind('input propertychange', function() {
 		$(".card .reference").text($("#reference-input").val());
